2acdf9d66c8b20225450308071fbc19cf49f0b45
The Chosen Lalafell <ddeeringwater@gmail.com>
2025-09-22 12:51:13 -0400

    Update marts.asm

--- data/items/marts.asm
+++ data/items/marts.asm
@@ -185,13 +185,14 @@ Goldenrod5FTMMart:
 	db -1
 
 GoldenrodHarborMart:
-	db 12 ; # items
+	db 13 ; # items
 	db ETHER
 	db ELIXIR
 	db DESTINY_KNOT
 	db SCOPE_LENS
 	db WIDE_LENS
 	db ZOOM_LENS
+	db EVERSTONE
 	db BRIGHTPOWDER
 	db LINKING_CORD
 	db MIRROR_HERB
@@ -265,7 +266,7 @@ MahoganyMart1:
 	db -1
 
 MahoganyMart2:
-	db 12 ; # items
+	db 11 ; # items
 	db RAGECANDYBAR
 	db AIR_BALLOON
 	db QUICK_CLAW
@@ -277,7 +278,6 @@ MahoganyMart2:
 	db SUPER_REPEL
 	db REVIVE
 	db FLOWER_MAIL
-	db ODD_SOUVENIR
 	db -1
 
 BlackthornMart:
