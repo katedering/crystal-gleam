3f3b8d9f0d2dd9bd885f18616c39a2602e60f889
The Chosen Lalafell <ddeeringwater@gmail.com>
2025-10-21 11:54:07 -0400

    Fix Shed Skin and held Berry both activate for the same status condition
    Port of 8093cf7 from Polished Crystal

--- engine/battle/abilities.asm
+++ engine/battle/abilities.asm
@@ -2065,9 +2065,18 @@ ShowAbilityActivation::
 	call PerformAbilityGFX
 	jmp PopBCDEHL
 
+ShowPotentialSpecificAbilityActivation:
+; ShowPotentialAbilityActivation if user's ability matches ability in a.
+	push bc
+	ld b, a
+	call GetTrueUserAbility
+	cp b
+	pop bc
+	ret nz
+
 ShowPotentialAbilityActivation:
 ; This avoids duplicating checks to avoid text spam. This will run
-; ShowAbilityActivation if animations are disabled (something only abilities do)
+; ShowAbilityActivation if we're within an ability execution (see BeginAbility).
 	ld a, [wInAbility]
 	and a
 	ret z
