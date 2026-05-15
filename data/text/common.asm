481ad4924c23b5a5eb94f2b95a3815ac6ab280f4
The Chosen Lalafell <ddeeringwater@gmail.com>
2026-05-15 18:44:35 -0400

    Add quantity selection for selling items to Item Maniacs
    port of https://github.com/Rangi42/polishedcrystal/commit/1ef93f2b5b91170e9403ef7748f855c4b0c6fd99 from PC
    
    Also ports "remove unused Minimize check"

--- data/text/common.asm
+++ data/text/common.asm
@@ -96,6 +96,13 @@ _HowManyShouldIMakeText::
 	line "make?"
 	done
 
+SECTION "_HowManyDoYouWantToSellText", ROMX
+_HowManyDoYouWantToSellText::
+	text "How many do you"
+	line "want to sell?"
+	done
+
+
 SECTION "_RecoveredSomeHPText", ROMX
 _RecoveredSomeHPText::
 	text_ram wStringBuffer1
